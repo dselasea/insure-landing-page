e3ec8162c141669f10d6895411ea146e42dd6e88
dselasea <dselasea@gmail.com>
2020-09-22 19:40:49 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+const close = document.getElementById("logo");
+const navigation = document.querySelector(".nav__navigation");
+const overlay = document.querySelector(".main-content");
+
+logo.addEventListener("click", function () {
+  navigation.classList.toggle("open");
+  overlay.classList.toggle("overlay");
+  console.log("Clicked");
+});
